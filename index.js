bae3206a8d00c79cd8c907c896481106ce63f4f6
Tiago Brito <tiago@avenuestud.io>
2019-07-04 17:45:32 +1000

    :boom: Added prefix for search term

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const readline = require("readline-sync");
 function start() {
   const content = {};
   content.searchTerm = askAndReturnSearchTerm();
+  content.prefix = askAndReturnPrefix();
 
   function askAndReturnSearchTerm() {
     return readline.question("Type a Wikipedia search term: ");
+  }
+  function askAndReturnPrefix() {
+    const prefixes = ["Who is", "What is", "THe history of"];
+    const selectedPrefixIndex = readline.keyInSelect(
+      prefixes,
+      "Choose one option : "
+    );
+    const selectedPrefixTest = prefixes[selectedPrefixIndex];
+
+    return selectedPrefixTest;
   }
 
   console.log(content);
